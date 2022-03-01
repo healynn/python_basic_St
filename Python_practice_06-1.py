507d727d1dc5fda3ba3a74328b48d1603fd2a06c
혜린 <87561227+healynn@users.noreply.github.com>
2022-03-01 08:39:12 +0000

    add code

--- Python_practice_06-1.py
+++ Python_practice_06-1.py
@@ -1,1 +1,22 @@
-print("happy")
+# 연습문제 06-1
+# 문제 1: 빈 리스트를 만들어 그 안에 1,2,3을 넣었다가 순서대로 꺼내는 코드 실행흐름
+"""
+st=[]
+st.append(1)
+st.append(2)
+st.append(3)
+print(st)
+st.remove(1)
+st.remove(2)
+st.remove(3)
+print(st)
+
+#문제 2
+st = [1,2,3]
+st.pop(0) #앞에서 부터 삭제 됨
+print(st)
+st.pop(-1)  #뒤에서 부터 삭제 됨
+"""
+#문제 3: for문을 이용하여 1-10까지 넣고 지우기 코드 제작
+st=[]
+for i in range[st]
