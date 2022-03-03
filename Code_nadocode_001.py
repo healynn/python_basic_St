3483609a920746fa9cf3cb6d9dcbd57ee8addbb1
혜린 <87561227+healynn@users.noreply.github.com>
2022-03-03 05:57:46 +0000

    add code

---
+++ Code_nadocode_001.py
@@ -0,0 +1,4 @@
+#나도코딩
+#퀴즈 3: 사이트별로 비밀번호를 만들어 주는 프로그램 작성하기
+ url= "http://naver.com"
+my_str= url.replace("http://")
--- main.sh
+++ main.sh
@@ -1,1 +1,1 @@
-python3 Python_practice_06-2.py
+python3 Python_practice_07-1.py
